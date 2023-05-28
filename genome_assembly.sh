b39cade06911731a43afe042a68abebc5b99fb2e
emiivo <100207998+emiivo@users.noreply.github.com>
2023-05-28 16:24:40 +0300

    remove Gepard analysis
    I decided to use a separate script for the next part of the homework.

--- genome_assembly.sh
+++ genome_assembly.sh
@@ -216,20 +216,3 @@ done
 # The mapping rates are higher for the SPAdes assemblies, indicating that a larger percentage 
 # of reads were successfully aligned to the reference genome using the SPAdes method. However, the coverage values are
 # higher for ABySSs assemblies, indicating a higher sequencing depth.
-
-
-# ---------------------------------------
-# Next step is using Gepard to create dotplots between samples. I did this through my own computer, 
-# results can be found in moodle.
-# I compared both SPAdes and ABySS scaffolds, but the I analyse AbySS assemblies, the more I believe my ABySS assemblies have either been made incorrectly
-# or I compromised them in a different step. The low mapping could an indicator of the assemblies not being correct. The dotplots do show coorelation,
-# but they are distorted and I am not sure they can even be analised properly.
-
-# As for the SPAdes samples, these are the results I got:
-# 1) ERR204044 and SRR15131330 - the dotplot indicates the sequences align in many areas, suggesting the sequences are similar.
-# This suggests that they may share common genomic regions or have similar genetic content.
-# 2) SRR18214264 and ERR204044 - the dotplot also shows that the sequences are similar, but they have more areas that differ in comparrisson to 
-# the ERR204044 and SRR15131330 dotplot. The differences observed in the dotplot suggest variations or divergent regions between the two samples,
-# indicating distinct genomic features or genetic differences.
-# 3) SRR18214264 and SRR15131330 - the dotplot is very similar to dotplot of ERR204044 and SRR15131330, indicating a high level of sequence 
-# alignment and similarity between these two samples. 
