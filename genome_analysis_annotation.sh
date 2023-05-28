20b4edcb3e0d99b5db2f2e8d41f27105af30d8e0
emiivo <100207998+emiivo@users.noreply.github.com>
2023-05-28 17:08:20 +0300

    Add Busco results

--- genome_analysis_annotation.sh
+++ genome_analysis_annotation.sh
@@ -14,3 +14,29 @@
 # indicating distinct genomic features or genetic differences.
 # 3) SRR18214264 and SRR15131330 - the dotplot is very similar to dotplot of ERR204044 and SRR15131330, indicating a high level of sequence 
 # alignment and similarity between these two samples. 
+
+# --------------------------------------
+# Next I analysed my genomes with BUSCO. I also did this through my own computer, through galaxy.eu.
+# The results of this analysis is further proving my point that there is something wrong with the ABySS assemblies, I uploaded the scaffolded data of all
+# six datasets (3 from ABySS and 3 from SPAdes). 
+
+# Firstly, the ABySS assemblies:
+# 1) ERR204044 - overall, the assembly is pretty good, 98.6% of the BUSCO groups were identified.
+# Out of the 402 BUSCO groups searched, 396 were complete, with 360 being single-copy and 36 being duplicated. 
+# Only 1% of the BUSCOs were fragmented, indicating that most of the identified genes were complete.
+
+# 2) SRR15131330 - many of the BUSCOs (15.2%) were fragmented, and most were not even found in the assembly (80.1%).
+# Only 4.7% of the BUSCOs in the lineage dataset were identified in the assembly. Overall, assembly failed the analysis.
+
+# 3) SRR18214264 - again, the majority of BUSCOs (60.4%) were not found in the assembly.
+# 21.4% of the BUSCOs were identified as fragmented, indicating partial gene sequences and only 18.2% of the BUSCOs were identified.
+
+# Overall, results of ABySS assemblies are not good.
+
+# Next, the SPAdes assemblies:
+# 1) ERR204044 - 99.0% of the BUSCOs in the lineage dataset were identified in the assembly. Only 0.5% of the BUSCOs were identified as fragmented, 
+# indicating nearly complete gene sequences and 0.5% were missing. This result is very good.
+# 2) SRR15131330 - 98.5% of the BUSCOs were identified in the assembly. Only 0.5% of the BUSCOs were identified as fragmented and 
+# only 1 of the BUSCOs were missing from the assembly. This result is also good.
+# 3) SRR18214264 - ad with ERR204044, 99.0% of the BUSCOs in the lineage dataset were identified in the assembly. Only 0.5% of the BUSCOs were identified as fragmented, 
+# indicating nearly complete gene sequences and 0.5% were missing. This result also very good.
